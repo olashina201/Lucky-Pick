3ca921a3d958a3d50edfad95931d7c54c50b705f
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-05-27 03:23:51 +0100

    update server folder

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-import express from 'express'
+const express = require('express')
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
 const app = express();
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors())
+
+const port = 5000;
+app.get("/", (req, res) => res.send("hello world"));
+
+app.listen(port, () => console.log(`server running on port${port}`));
