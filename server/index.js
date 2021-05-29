967b928fc3ee1d1b7b74527e3adc0e18e48afe51
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-05-29 04:45:21 +0100

    install express

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
+const app = express()
+const port = 5000
 
-const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const port = 5000;
-app.get("/", (req, res) => res.send("hello world"));
-
-app.listen(port, () => console.log(`server running on port${port}`));
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
